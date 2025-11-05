70886738328306a862ed0e1bdf6c660efd626109
Raffaele <r.sbardella@studenti.unisa.it>
2025-11-06 00:54:37 +0100

    debug app-public 2

--- app-public.py
+++ app-public.py
@@ -274,7 +274,10 @@ if prompt := st.chat_input(ui_texts["chat_input_placeholder"]):
 
         # Scrivi lo stream sul frontend e cattura la risposta completa
         final_response_text = st.write_stream(streaming_response.response_gen)
+        if final_response_text is None:
+            final_response_text = "NIENTEEE"
         print("Final response text:", final_response_text)
+        st.write(final_response_text)
 
         source_nodes_for_display = streaming_response.source_nodes
 
